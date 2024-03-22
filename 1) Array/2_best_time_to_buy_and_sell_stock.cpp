5c77ed30158b38f96977d8b1c900e80507480921
Pratham Gupta <prathamgupta.pg1@gmail.com>
2024-03-22 21:54:13 +0530

    Q2: Best Time to Buy and Sell Stock

---
+++ 1) Array/2_best_time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int l = 0, r = 1;
+        int ans = 0;
+
+        while (r < prices.size())
+        {
+            if (prices[r] < prices[l])
+                l = r;
+            else
+                ans = max(ans, prices[r] - prices[l]);
+
+            r++;
+        }
+
+        return ans;
+    }
+};
