634a016860b222a96da6cea5482a355220ca5ed3
Pratham Gupta <prathamgupta.pg1@gmail.com>
2024-03-23 22:42:45 +0530

    Q6: Maximum Product Subarray

---
+++ 1) Array/6_maximum_product_subarray.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProduct(vector<int> &nums)
+    {
+        int ans = INT_MIN, prefix = 1, suffix = 1;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            if (prefix == 0)
+                prefix = 1;
+
+            if (suffix == 0)
+                suffix = 1;
+
+            prefix *= nums[i];
+            suffix *= nums[n - 1 - i];
+
+            ans = max(ans, max(prefix, suffix));
+        }
+
+        return ans;
+    }
+};
